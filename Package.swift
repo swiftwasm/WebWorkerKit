acd6a0988269fb79580a5c73046aff63691196c0
Yuta Saito <kateinoigakukun@gmail.com>
2023-02-02 17:28:22 +0900

    `Distributed` module is only available on those versions
    This requirement is noop when building for WebAssembly target, but it's
    required to develop with Xcode

--- Package.swift
+++ Package.swift
@@ -3,6 +3,12 @@ import PackageDescription
 
 let package = Package(
     name: "WebWorkerKit",
+    platforms: [
+        .iOS(.v16),
+        .macOS(.v13),
+        .tvOS(.v16),
+        .watchOS(.v9),
+    ],
     products: [
         .library(
             name: "WebWorkerKit",
