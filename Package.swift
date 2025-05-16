ee2b4e56e22dc0dde150c0cde4801d410658fc5b
reckjn <julian.reck@gmx.net>
2025-05-16 12:43:39 +0200

    bump min versions for macOS to 10.15 and iOS to 13

--- Package.swift
+++ Package.swift
@@ -3,6 +3,10 @@ import PackageDescription
 
 let package = Package(
     name: "WebWorkerKit",
+    platforms: [
+        .macOS("10.15"),
+        .iOS(.v13),
+    ],
     products: [
         .library(
             name: "WebWorkerKit",
