7de58371b78a3c53b0e70c339f9ba9623b781890
Julian Reck <julian.reck@gmx.net>
2026-04-10 10:54:13 +0200

    Bump minimum Mac version to 13.0 (#3)

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "WebWorkerKit",
-    platforms: [.macOS(.v10_15), .iOS(.v13)],
+    platforms: [.macOS(.v13), .iOS(.v13)],
     products: [
         .library(
             name: "WebWorkerKit",
